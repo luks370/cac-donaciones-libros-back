4127bdbac29c0c297a2589b2e1dcd53b3f977454
Lucas Eduardo Ovejero <cursos849@gmail.com>
2024-07-07 21:56:04 -0300

    creamos el archivo librosRouter

---
+++ routers/libros.router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const librosController = require("../controllers/librosController")
+
+
+// Obtener todos los libros publicados
+router.get("/", librosController.getAllLibros)
+
+// Publicar un libro nuevo
+router.post("/", librosController.postLibro)
+
+// Actualizar un libro publicado
+router.put("/:id", librosController.putLibroById)
+
+// Borrar un libro publicado
+router.delete("/:id", librosController.deleteLibroById)
+
+
+module.exports = router
